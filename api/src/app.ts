ab589d7ce68587c5832db618b624f84788da23c8
Valentin Manceaux-Panot <valentin@mcxp.io>
2026-01-17 18:09:15 +0100

    Preparing the API for Vercel deployment

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,29 @@
-// This is for Vercel
-import { app } from "./http";
+import express from "express";
+import googleRouter from "./http/google";
+import graphqlRouter from "./http/graphql";
+import cors from "cors";
 
+export const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+
+app.get("/health", (request, response) => {
+  response.status(200).json({ healthy: true });
+});
+app.post("/health", (request, response) => {
+  response.status(200).json({ healthy: true });
+});
+
+app.use("/google", googleRouter);
+app.use("/graphql", graphqlRouter);
+
+export const api = (): Promise<void> =>
+  new Promise((resolve) => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server running on port", process.env.PORT);
+      resolve();
+    });
+  });
+
+// This is for Vercel
 export default app;
--- api/src/dev.ts
+++ api/src/dev.ts
@@ -1,4 +1,4 @@
-import { api } from "./http";
+import { api } from "./app";
 import { sync } from "./sync";
 
 api().catch(console.error);

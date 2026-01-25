480839c20313a06764f8bdacd484bdd220541f4b
Valentin Manceaux-Panot <valentin@mcxp.io>
2026-01-25 11:59:12 +0100

    Added Youtube thumbnail cropping

--- api/src/http/thumbnail.ts
+++ api/src/http/thumbnail.ts
@@ -3,6 +3,9 @@ import validate from "express-zod-safe";
 import { z } from "zod";
 import { db } from "../database";
 import { Readable } from "node:stream";
+import sharp from "sharp";
+import { SourceType } from "../generated/prisma";
+import { ReadableStream } from "node:stream/web";
 
 const thumbnailRouter = express.Router();
 
@@ -20,11 +23,36 @@ thumbnailRouter.get(
       case "item":
         const item = await db.item.findUniqueOrThrow({
           where: { id: request.params.id },
-          select: { thumbnailUrl: true },
+          select: { thumbnailUrl: true, source: { select: { type: true } } },
         });
         url = item.thumbnailUrl;
         // 7 days
         response.setHeader("cache-control", "max-age=0, s-maxage=604800");
+
+        if (url && item.source.type === SourceType.Youtube) {
+          const imageResponse = await fetch(url);
+          response.setHeader(
+            "Content-Type",
+            imageResponse.headers.get("content-type")!,
+          );
+          const image = await imageResponse.arrayBuffer();
+
+          const sharpImage = sharp(image);
+          const processing = await sharpImage.metadata().then((metadata) => {
+            const rawCroppedHeight = Math.floor(metadata.width / (16 / 9));
+            const margin = Math.ceil((metadata.height - rawCroppedHeight) / 2);
+
+            return sharpImage.extract({
+              top: margin,
+              height: metadata.height - 2 * margin,
+              left: 0,
+              width: metadata.width,
+            });
+          });
+          processing.pipe(response);
+          return;
+        }
+
         break;
       case "source":
         const source = await db.source.findUniqueOrThrow({
@@ -45,8 +73,8 @@ thumbnailRouter.get(
     const image = await fetch(url);
 
     response.setHeader("Content-Type", image.headers.get("content-type")!);
-    // @ts-expect-error This works 🤷
-    Readable.fromWeb(image.body!).pipe(response);
+
+    Readable.fromWeb(image.body as ReadableStream<any>).pipe(response);
   },
 );
 
